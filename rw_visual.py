23ca84a96376ca705f58e764c0e2a054f78f93b7
dashenwkp <2126879457@qq.com>
2026-06-25 22:06:36 +0800

    随机游走

--- rw_visual.py
+++ rw_visual.py
@@ -2,11 +2,24 @@ from random_walk import RandomWalk
 
 import matplotlib.pyplot as plt
 
-rw = RandomWalk()
-rw.fill_work()
+while True:
+    rw = RandomWalk()
+    rw.fill_work()
 
-plt.style.use('classic')
-fig, ax = plt.subplots()
-ax.scatter(rw.x_values, rw.y_values, s=15)
-ax.set_aspect('equal')
-plt.show()
+    plt.style.use('classic')
+    fig, ax = plt.subplots(figsize=(15, 9))
+    point_numbers = range(rw.num_points)
+    ax.plot(rw.x_values, rw.y_values, linewidth=3)
+    ax.set_aspect('equal')
+
+    ax.scatter(0, 0, c='green', edgecolors='none', s=100)
+    ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red',
+                edgecolors='none', s=100)
+    
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+    plt.show()
+
+    keep_running = input('Make another walk? (y/n)')
+    if keep_running == 'n':
+        break
